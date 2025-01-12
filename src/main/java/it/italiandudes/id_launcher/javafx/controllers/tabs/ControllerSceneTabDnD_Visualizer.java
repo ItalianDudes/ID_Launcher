8a6fcddca3ff2b220ab7bb5055cc2687d888c43e
Hacker6329 <Hacker6329@gmail.com>
2025-01-12 14:17:57 +0100

    Added browse and delete version

--- src/main/java/it/italiandudes/id_launcher/javafx/controllers/tabs/ControllerSceneTabDnD_Visualizer.java
+++ src/main/java/it/italiandudes/id_launcher/javafx/controllers/tabs/ControllerSceneTabDnD_Visualizer.java
@@ -3,7 +3,9 @@ package it.italiandudes.id_launcher.javafx.controllers.tabs;
 import it.italiandudes.id_launcher.enums.LauncherBehaviour;
 import it.italiandudes.id_launcher.javafx.Client;
 import it.italiandudes.id_launcher.javafx.JFXDefs;
+import it.italiandudes.id_launcher.javafx.alerts.ConfirmationAlert;
 import it.italiandudes.id_launcher.javafx.alerts.ErrorAlert;
+import it.italiandudes.id_launcher.javafx.alerts.InformationAlert;
 import it.italiandudes.id_launcher.javafx.controllers.ControllerSceneMainMenu;
 import it.italiandudes.id_launcher.javafx.utils.Settings;
 import it.italiandudes.id_launcher.release.IDRelease;
@@ -17,9 +19,16 @@ import it.italiandudes.idl.common.Logger;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.ClipboardContent;
+import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -58,6 +67,8 @@ public final class ControllerSceneTabDnD_Visualizer {
     @FXML private Label labelDownload;
     @FXML private Button buttonDownload;
     @FXML private ComboBox<IDVersion> comboBoxVersionSelector;
+    @FXML private Button buttonBrowse;
+    @FXML private Button buttonDelete;
     @FXML private Button buttonStart;
     @FXML private Button buttonRefresh;
 
@@ -67,46 +78,53 @@ public final class ControllerSceneTabDnD_Visualizer {
         JFXDefs.startServiceTask(() -> {
             //noinspection StatementWithEmptyBody
             while (!configurationComplete);
-            try {
-                postInitialize();
-            } catch (IOException e) {
-                Logger.log(e);
-            }
+            postInitialize();
         });
     }
-    private void postInitialize() throws IOException { // Non-EDT Initialize
-        Platform.runLater(this::resetController);
-        refreshReleases();
-        File[] versionDirs = INSTALLATION_DIR.listFiles();
-        ArrayList<@NotNull File> jars = new ArrayList<>();
-        if (versionDirs != null) {
-            Arrays.stream(versionDirs).forEach(version -> {
-                File[] jarFiles = version.listFiles((dir, name) -> name.startsWith(APP_NAME) && name.endsWith(".jar"));
-                if (jarFiles != null && jarFiles.length > 0) jars.addAll(Arrays.asList(jarFiles));
-            });
-        }
-        String latestFilename = latest != null?latest.getFilename():null;
-        if (jars.isEmpty() || jars.stream().map(File::getName).noneMatch(Predicate.isEqual(latestFilename))) {
-            Platform.runLater(() -> {
-                buttonDownload.setDisable(false);
-                buttonDownload.setVisible(true);
-            });
-        } else {
-            List<File> launchableJarList = new ArrayList<>();
-            for (File jar : jars) {
-                Attributes manifest = JarHandler.ManifestReader.readJarManifest(jar);
-                if (JarHandler.ManifestReader.containsKey(manifest, "ID-Launcher-Enabled") && JarHandler.ManifestReader.getValue(manifest, "ID-Launcher-Enabled").equals("true")) {
-                    launchableJarList.add(jar);
-                }
+    private void postInitialize() { // Non-EDT Initialize
+        try {
+            Platform.runLater(this::resetController);
+            refreshReleases();
+            File[] versionDirs = INSTALLATION_DIR.listFiles();
+            ArrayList<@NotNull File> jars = new ArrayList<>();
+            if (versionDirs != null) {
+                Arrays.stream(versionDirs).forEach(version -> {
+                    File[] jarFiles = version.listFiles((dir, name) -> name.startsWith(APP_NAME) && name.endsWith(".jar"));
+                    if (jarFiles != null && jarFiles.length > 0) jars.addAll(Arrays.asList(jarFiles));
+                });
             }
+            String latestFilename = latest != null ? latest.getFilename() : null;
+            if (jars.isEmpty() || jars.stream().map(File::getName).noneMatch(Predicate.isEqual(latestFilename))) {
+                Platform.runLater(() -> {
+                    buttonDownload.setDisable(false);
+                    buttonDownload.setVisible(true);
+                });
+            } else {
+                List<File> launchableJarList = new ArrayList<>();
+                for (File jar : jars) {
+                    Attributes manifest = JarHandler.ManifestReader.readJarManifest(jar);
+                    if (JarHandler.ManifestReader.containsKey(manifest, "ID-Launcher-Enabled") && JarHandler.ManifestReader.getValue(manifest, "ID-Launcher-Enabled").equals("true")) {
+                        launchableJarList.add(jar);
+                    }
+                }
+                Platform.runLater(() -> {
+                    buttonDownload.setDisable(true);
+                    buttonDownload.setVisible(false);
+                    buttonBrowse.setVisible(true);
+                    buttonDelete.setVisible(true);
+                    buttonStart.setVisible(true);
+                    comboBoxVersionSelector.getItems().addAll(launchableJarList.stream().map(IDVersion::new).collect(Collectors.toList()));
+                    comboBoxVersionSelector.getSelectionModel().selectFirst();
+                    comboBoxVersionSelector.setVisible(true);
+                });
+            }
+        } catch (IOException e) {
+            Logger.log(e);
             Platform.runLater(() -> {
-                buttonDownload.setDisable(true);
-                buttonDownload.setVisible(false);
-                buttonStart.setVisible(true);
-                comboBoxVersionSelector.getItems().addAll(launchableJarList.stream().map(IDVersion::new).collect(Collectors.toList()));
-                comboBoxVersionSelector.getSelectionModel().selectFirst();
-                comboBoxVersionSelector.setVisible(true);
+                new ErrorAlert("ERRORE", "Errore di I/O", "Si e' verificato un errore durante la ricarica dell'interfaccia, chiusura...");
+                JFXDefs.startServiceTask(Client::exit);
             });
+
         }
     }
 
@@ -121,6 +139,10 @@ public final class ControllerSceneTabDnD_Visualizer {
         progressBarDownload.setVisible(false);
         buttonDownload.setVisible(true);
         buttonDownload.setDisable(true);
+        buttonBrowse.setVisible(false);
+        buttonBrowse.setDisable(true);
+        buttonDelete.setVisible(false);
+        buttonDelete.setDisable(true);
         buttonStart.setVisible(false);
         buttonStart.setDisable(true);
     }
@@ -135,7 +157,7 @@ public final class ControllerSceneTabDnD_Visualizer {
     private void displayLatestRelease() {
         if (latest == null) return;
         Platform.runLater(() -> {
-            textFieldReleaseTitle.setText(latest.getReleaseTitle());
+            textFieldReleaseTitle.setText("[" + latest.getVersion() + "] " + latest.getReleaseTitle());
             textAreaPatchNotes.setText(latest.getPatchNotes());
         });
     }
@@ -147,17 +169,16 @@ public final class ControllerSceneTabDnD_Visualizer {
         resetController();
         JFXDefs.startServiceTask(() -> {
             refreshReleases();
-            try {
-                postInitialize();
-            } catch (IOException e) {
-                Logger.log(e);
-            }
+            postInitialize();
             Platform.runLater(() -> buttonRefresh.setDisable(false));
         });
     }
     @FXML
     private void versionChanged() {
-        buttonStart.setDisable(comboBoxVersionSelector.getSelectionModel().getSelectedItem() == null);
+        boolean disable = comboBoxVersionSelector.getSelectionModel().getSelectedItem() == null;
+        buttonBrowse.setDisable(disable);
+        buttonDelete.setDisable(disable);
+        buttonStart.setDisable(disable);
     }
     @FXML
     private void startDownload() {
@@ -191,9 +212,72 @@ public final class ControllerSceneTabDnD_Visualizer {
         });
     }
     @FXML
+    private void browseVersion() {
+        IDVersion selectedVersion = comboBoxVersionSelector.getSelectionModel().getSelectedItem();
+        if (selectedVersion == null) {
+            new ErrorAlert("ERRORE", "Errore di Selezione", "Selezionare prima una versione.");
+            return;
+        }
+        String version = selectedVersion.getParentFile().getName();
+        if (Desktop.isDesktopSupported()) {
+            JFXDefs.startServiceTask(() -> {
+                try {
+                    Desktop.getDesktop().browse(selectedVersion.getParentFile().toURI());
+                } catch (UnsupportedOperationException e) {
+                    Logger.log(e);
+                    Platform.runLater(() -> {
+                        new ErrorAlert("ERRORE", "Sistema Incompatibile", "Questa piattaforma non e' compatibile per l'apertura della cartella, il percorso alla cartella e' stato copiato nella clipboard di sistema.\nPercorso Cartella: " + selectedVersion.getParentFile().getAbsolutePath());
+                        ClipboardContent content = new ClipboardContent();
+                        content.putString(selectedVersion.getParentFile().getAbsolutePath());
+                        Client.getSystemClipboard().setContent(content);
+                    });
+                } catch (IllegalArgumentException e) {
+                    Logger.log(e);
+                    Platform.runLater(() -> new ErrorAlert("ERRORE", "Permessi Mancanti", "Mancano i permessi per aprire la cartella della versione \"" + version + "\"."));
+                } catch (IOException e) {
+                    Logger.log(e);
+                    // default browser is not found, or it fails to be launched, or the default handler application failed to be launched
+                    Platform.runLater(() -> new ErrorAlert("ERRORE", "Errore di I/O", "Non e' stato possibile trovare il File Browser di sistema, oppure si e' verificato un errore durante l'apertura o con il Default Handler Application."));
+                }
+            });
+        } else {
+            new ErrorAlert("ERRORE", "Sistema Incompatibile", "Questa desktop non e' compatibile per l'apertura della cartella, il percorso alla cartella e' stato copiato nella clipboard di sistema.\nPercorso Cartella: " + selectedVersion.getParentFile().getAbsolutePath());
+            ClipboardContent content = new ClipboardContent();
+            content.putString(selectedVersion.getParentFile().getAbsolutePath());
+            Client.getSystemClipboard().setContent(content);
+        }
+    }
+    @FXML
+    private void deleteVersion() {
+        IDVersion selectedVersion = comboBoxVersionSelector.getSelectionModel().getSelectedItem();
+        if (selectedVersion == null) {
+            new ErrorAlert("ERRORE", "Errore di Selezione", "Selezionare prima una versione.");
+            return;
+        }
+        String version = selectedVersion.getParentFile().getName();
+        boolean delete = new ConfirmationAlert("ELIMINA?", "Conferma Eliminazione", "Vuoi davvero eliminare la versione \"" + version + "\"?\nTutti i file contenuti nella sua cartella verranno eliminati!").result;
+        if (!delete) return;
+        JFXDefs.startServiceTask(() -> {
+            try {
+                System.gc(); // Needed to clear some pointers that locks target files
+                FileUtils.deleteDirectory(selectedVersion.getParentFile());
+                Platform.runLater(() -> new InformationAlert("SUCCESSO", "Eliminazione Completata", "Versione \"" + version + "\" eliminata con successo!"));
+            } catch (IOException e) {
+                Logger.log(e);
+                Platform.runLater(() -> new ErrorAlert("ERRORE", "Errore di Eliminazione", "Si e' verificato un errore durante l'eliminazione della versione \"" + version + "\", verifica che non sia stata gia' eliminata o che non sia aperta da questo o un altro processo."));
+            }
+            postInitialize();
+        });
+    }
+    @FXML
     private void startVersion() {
         IDVersion version = comboBoxVersionSelector.getSelectionModel().getSelectedItem();
-        if (version == null) return;
+        if (version == null) {
+            new ErrorAlert("ERRORE", "Errore di Selezione", "Selezionare prima una versione.");
+            return;
+        }
+        buttonBrowse.setDisable(true);
+        buttonDelete.setDisable(true);
         buttonStart.setDisable(true);
         JFXDefs.startServiceTask(() -> {
             URLClassLoader classLoader = null;
@@ -206,6 +290,8 @@ public final class ControllerSceneTabDnD_Visualizer {
                 } catch (NoSuchMethodException e) {
                     Platform.runLater(() -> {
                         new ErrorAlert("ERRORE", "Errore di Versione", "Questa versione non supporta l'avvio da launcher.");
+                        buttonBrowse.setDisable(false);
+                        buttonDelete.setDisable(false);
                         buttonStart.setDisable(false);
                     });
                     Logger.log("This app version doesn't support launcher start!", new InfoFlags(true, false));
@@ -261,7 +347,11 @@ public final class ControllerSceneTabDnD_Visualizer {
                     Logger.log(e);
                 }
             }
-            Platform.runLater(() -> buttonStart.setDisable(false));
+            Platform.runLater(() -> {
+                buttonBrowse.setDisable(false);
+                buttonDelete.setDisable(false);
+                buttonStart.setDisable(false);
+            });
         });
     }
 }
